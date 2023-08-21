24d2f4d8fde31a7eed8d1d4006f9e8a2f8fc353b
Rafael Lobo <126676785+lrafa10z@users.noreply.github.com>
2023-08-21 21:24:32 +0100

    Add files via upload

---
+++ 5-atribuicoesDeVariaveis.js
@@ -0,0 +1,15 @@
+console.log("Trabalhando com Atribuição de variáveis")
+
+const idade = 29;
+let primeiroNome = "Ricardo";
+const sobrenome = "Bugan";
+
+//console.log(nome + " " sobrenome)
+console.log(nome, sobrenome);
+console.log(`Meu nome é ${nome} ${sobrenome}`);
+
+const nomeCompleto = nome + sobrenome;
+console.log(nomeCompleto);
+
+
+
