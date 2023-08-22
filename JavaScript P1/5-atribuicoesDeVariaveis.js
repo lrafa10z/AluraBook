7589f6e17ee499ed9c477daf3d45f51f2269fad1
Rafael Lobo <126676785+lrafa10z@users.noreply.github.com>
2023-08-22 18:01:59 -0300

    Add files via upload

--- JavaScript P1/5-atribuicoesDeVariaveis.js
+++ JavaScript P1/5-atribuicoesDeVariaveis.js
@@ -1,15 +1,18 @@
-console.log("Trabalhando com Atribuição de variáveis")
-
-const idade = 29;
-let primeiroNome = "Ricardo";
+console.log("Trabalhando com atribuição de variáveis");
+const primeiroNome = "Ricardo";
 const sobrenome = "Bugan";
 
-//console.log(nome + " " sobrenome)
-console.log(nome, sobrenome);
-console.log(`Meu nome é ${nome} ${sobrenome}`);
+//console.log(nome + " " + sobrenome)
+console.log(primeiroNome, sobrenome);
+console.log(`Meu nome é ${primeiroNome} ${sobrenome}`);
 
-const nomeCompleto = nome + sobrenome;
+let contador = 0;
+contador = contador +1;
+
+const nomeCompleto = primeiroNome + sobrenome;
 console.log(nomeCompleto);
 
-
-
+let idade; //declarando variável
+idade = 26; //atribuindo valor
+idade = idade + 1;
+console.log(idade)
